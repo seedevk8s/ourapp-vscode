a932a59f0341bd604a242687bd7ec5eaf1b317a9
seedevk8s <seedevk8s@gmail.com>
2024-09-23 16:10:43 +0900

    뷰 추가

--- src/main/java/com/kosta/ourapp/controller/HomeController.java
+++ src/main/java/com/kosta/ourapp/controller/HomeController.java
@@ -1,5 +1,15 @@
 package com.kosta.ourapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+  @GetMapping("/home")      // new HomeController().home()
+  public String home(Model model) {
+    return "index"; // index.html   
+  }
 
 }
